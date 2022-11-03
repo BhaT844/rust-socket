e6e9fb42dfa2e0fdc0ca85020d9f61f713ff1aca
BhaT844 <bluehart_777@naver.com>
2022-11-03 17:05:15 +0900

    socket

---
+++ client/src/main.rs
@@ -0,0 +1,45 @@
+use std::net::TcpStream;
+use std::io::{self, Read, Write};
+use std::process;
+use std::str::from_utf8;
+
+fn main() {
+    match TcpStream::connect("localhost:3333") {
+        Ok(mut stream) => {
+            println!("성공적으로 서버에 연결되었습니다");
+
+            loop {
+                let mut message = String::new();
+                let     message = input(&mut message);
+
+                if message == b"/quit" {
+                    break;
+                }
+
+                stream.write(message).unwrap_or_else(|err| {
+                    println!("데이터 전송 실패: {}", err);
+                    process::exit(1);
+                });
+
+                let mut buffer = [0_u8; 50];
+                match stream.read(&mut buffer) {
+                    Ok(_) => {
+                        let text = from_utf8(&buffer).unwrap();
+                        println!("수신: {}", text);
+                    },
+                    Err(e) => {
+                        println!("데이터 수신 실패: {}", e);
+                    },
+                }
+            }
+        },
+        Err(e) => {
+            println!("연결에 실패하였습니다: {}", e);
+        },
+    }
+}
+
+fn input<'a>(msg: &'a mut String) -> &'a [u8] {
+    io::stdin().read_line(msg).unwrap();
+    msg.as_bytes()
+}
---
+++ server/src/main.rs
@@ -0,0 +1,62 @@
+use std::{thread, process};
+use std::net::{TcpListener, Shutdown, TcpStream};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
+fn main() {
+    const PORT: &str = "3333";
+    let addr = format!("0.0.0.0:{}", PORT);
+
+    let listener = TcpListener::bind(addr).unwrap_or_else(|err| {
+        eprintln!("서버 생성 실패: {}", err);
+        process::exit(1);
+    });
+
+    println!("{}번 포트에서 서버가 열렸습니다.", PORT);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) =>  {
+                println!("{} 연결됨", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            },
+            Err(e) => {
+                println!("연결실패: {}", e);
+            },
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0_u8; 50];
+
+    while match stream.read(&mut buffer) {
+        Ok(size) => {
+            println!("{}: {}", stream.peer_addr().unwrap(), from_utf8(&buffer).unwrap());
+
+            for i in buffer.iter_mut() {
+                if
+                    65    <=    *i && *i    <=    90
+                {
+                    *i += 32;
+                }
+
+                else if
+                    97    <=    *i && *i    <=    122
+                {
+                    *i -= 32;
+                }
+            }
+
+            stream.write(&buffer[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("오류가 발생하여 연결을 종료합니다: {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        },
+    } {}
+}
